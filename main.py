bebff0254821cb11f4e271ce6021f345a1423b6f
Kostiantin Gonchar <gokostik@gmail.com>
2021-04-25 21:23:25 +0300

    all done

--- main.py
+++ main.py
@@ -1,92 +1,122 @@
 from turtle import *
 import random
 
-movespeed = 1
+is_draw = True
 gogogo = True
+movespeed = 1
 n = 0
 
 timy = Turtle()
 timy.shape("arrow")
 timy.hideturtle()
 colormode(255)
-timy.pencolor(222, 123, 255)
+timy.pencolor(222,123,255)
 timy.pensize(1)
 timy.shapesize(2)
 timy.speed(3)
 
 screen = Screen()
 
-# set size:
+  #set size:
 
-screen.setup(width=1.0, height=1.0)
+screen.setup(width = 1.0, height = 1.0)
 
-# remove close,minimaze,maximaze buttons:
+#remove close,minimaze,maximaze buttons:
 canvas = screen.getcanvas()
 root = canvas.winfo_toplevel()
 root.overrideredirect(1)
 
 textout = Turtle()
 textout.up()
-textout.setpos(-1980 / 2 + 50, 1050 / 2 - 20)
+textout.setpos(-1980/2+50, 1050/2-20)
 textout.write("Q - Heart    W - Flour   ] - Quit", font=("Arial", 16, "normal"))
 
 move = Turtle()
 
+def showpos():
+    textout.up()
+    textout.setpos(-1980 / 2 + 50, 1050 / 2 - 105)
+    textout.down()
+    textout.pencolor(255, 255, 255)
+    textout.write("███████████████████████████████", font=("Arial", 24, "normal"))
+    textout.pencolor(0, 0, 0)
+    textout.write(f"{move.pos()} speed {movespeed}", font=("Arial", 24, "normal"))
 
-def k01():
-    movespeed = movespeed + 1
+def tocenter():
+    move.setpos(0, 0)
+    showpos()
+
+def kvadrat():
+    move.down()
+    move.forward(150)
+    move.left(90)
+    move.forward(150)
+    move.left(90)
+    move.forward(150)
+    move.left(90)
+    move.forward(150)
+    move.up()
+    showpos()
+
+def speedup():
+    global movespeed
+    movespeed = movespeed+1
     move.speed(movespeed)
+    showpos()
 
-
-def k02():
-    movespeed = movespeed - 1
+def speeddown():
+    global movespeed
+    movespeed = movespeed-1
     move.speed(movespeed)
-
+    showpos()
 
 def k0():
     quit()
 
-
 def k1():
     move.forward(5)
-
+    showpos()
 
 def k2():
     move.left(5)
-
+    showpos()
 
 def k3():
     move.right(5)
-
+    showpos()
 
 def k4():
     move.back(5)
-
+    showpos()
 
 def k5():
     move.down()
     move.left(45)
     move.forward(105)
-    move.circle(50, 200)
+    move.circle(50,200)
     move.right(125)
-    move.circle(50, 200)
+    move.circle(50,200)
     move.forward(105)
     move.up()
-
+    showpos()
 
 def k6():
     global gogogo, n
-    gogogo = True
+    gogogo=True
     move.speed(0)
     if n >= 1000:
-        n = 0
+        n=0
 
-    textout.setpos(-1980 / 2 + 50, 1050 / 2 - 70)
+
     while (n < 1000) and (gogogo):
         listen()
-        textout.pencolor(255, 255, 255)
-        textout.write("██████ [get some bread]", font=("Arial", 24, "normal"))
-        textout.pencolor(0, 0, 0)
+        showpos()
+        textout.up()
+        textout.setpos(-1980 / 2 + 50, 1050 / 2 - 70)
+        textout.down()
+        textout.pencolor(255,255,255)
+        textout.write( "██████ [get some bread]", font=("Arial", 24, "normal"))
+        textout.pencolor(0,0,0)
         textout.write(n, font=("Arial", 24, "normal"))
         move.down()
         move.circle(50, 201)
@@ -99,23 +129,30 @@ def k6():
         move.right(129)
         move.circle(50, 201)
         move.up()
-        n = n + 1
-
+        n=n+1
 
 def k7():
-    move.down()
+    global is_draw
+    if is_draw:
+        move.up()
+    else:
+        move.down()
+    is_draw = not is_draw
 
 
 def k00():
     global gogogo
-    gogogo = False
+    gogogo=False
 
+def fxn(x, y):
+    move.goto(x, y)
+    showpos()
 
-onkeypress(k01, ",")
-onkeypress(k02, ".")
-
+onclick(fxn)
 onkeypress(k0, "]")
 onkeypress(k00, "[")
+onkeypress(speedup, ".")
+onkeypress(speeddown, ",")
 onkeypress(k1, "Up")
 onkeypress(k2, "Left")
 onkeypress(k3, "Right")
@@ -123,6 +160,14 @@ onkeypress(k4, "Down")
 onkeypress(k5, "q")
 onkeypress(k6, "w")
 onkeypress(k7, "space")
+onkeypress(kvadrat, "s")
+onkeypress(tocenter, "z")
+onkeypress(showpos, "p")
+onkeypress(screen.reset, "/")
+
+
+
+
 
 listen()
 mainloop()
@@ -131,9 +176,9 @@ while True:
     timy.begin_fill()
     timy.left(45)
     timy.forward(105)
-    timy.circle(50, 200)
+    timy.circle(50,200)
     timy.right(125)
-    timy.circle(50, 200)
+    timy.circle(50,200)
     timy.forward(105)
     timy.up()
 
